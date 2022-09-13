56be2466cfc6b5bf677063b2f73f9cf4d97dfefe
xlfguang <xlfguang@users.noreply.github.com>
2022-09-13 11:42:01 +0000

    deploy: 121a0f86072e3c028f7fe1f46152528f1a8e8bfc

---
+++ _next/static/chunks/ae51ba48-9b380ca0f45298a3fdf0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{60155:function(t,n,e){"use strict";e.d(n,{dUf:function(){return i}});var r=e(44405);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"48",d:"M328 112L184 256l144 144"}}]})(t)}}}]);
