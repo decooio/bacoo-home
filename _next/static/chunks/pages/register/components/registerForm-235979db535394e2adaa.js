a325b2cbeb9b8ace597d53bd34e004822b08015c
xlfguang <xlfguang@users.noreply.github.com>
2022-09-15 17:13:08 +0000

    deploy: 45fee6b5eb439f9270f9207d99190911c9e7ec27

---
+++ _next/static/chunks/pages/register/components/registerForm-235979db535394e2adaa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[634],{22552:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/register/components/registerForm",function(){return e(82193)}])}},function(n){n.O(0,[102,912,2,193,774,888,179],(function(){return _=22552,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/resetPassword-5d43fec31b71fecf4c42.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[488],{2894:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/resetPassword",function(){return u(85832)}])}},function(n){n.O(0,[102,912,2,193,832,774,888,179],(function(){return _=2894,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
