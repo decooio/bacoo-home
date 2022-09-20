6558b4906a865bb461713e1239da098f1b72d1ba
xlfguang <2684695148@qq.com>
2022-09-20 17:37:58 +0800

    fix:样式修复

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,27 +1,40 @@
-import { FormWrapper } from "@pages/signup";
 import React from "react";
 import styled from "styled-components";
 import { getLogoDark } from "../../src/helper/utils";
 import RegisterForm from "./components/registerForm";
-
+const RegisterBox = styled.div`
+  @media (max-height: 800px) {
+    align-items: end;
+  }
+`;
 const Logo = styled.embed`
   width: 165px;
-  //height: 55px;
+
   margin-bottom: 63px;
   @media (max-height: 800px) {
     margin-bottom: 20px;
   }
-  @media (max-height: 650px) {
+  @media (max-height: 680px) {
     display: none;
   }
 `;
+export const FormWrapper = styled.div`
+  @media (min-width: 768px) {
+    width: 457px;
+  }
+`;
+const LogoBox = styled.div`
+  width: 100%;
+`;
 export default function Register() {
   return (
-    <div className="w-full h-full flex relative items-center justify-center">
+    <RegisterBox className="w-full h-full flex relative items-center justify-center">
       <FormWrapper className="relative flex  flex-col	items-center w-full px-8 md:px-0">
-        <Logo src={getLogoDark()} />
+        <LogoBox className="flex items-center justify-center">
+          <Logo src={getLogoDark()} />
+        </LogoBox>
         <RegisterForm />
       </FormWrapper>
-    </div>
+    </RegisterBox>
   );
 }
