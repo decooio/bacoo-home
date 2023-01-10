bba286a04db8b18bde32035b3f2ea7a5024f8980
eericxu <2681350846@qq.com>
2023-01-10 13:44:09 +0800

    set auth for zhong

--- src/helper/const.ts
+++ src/helper/const.ts
@@ -31,11 +31,11 @@ function createOemConfig(): OEMConfig {
       logoDark: '/zhong/logo_dark.png',
       fav: "/zhong/fav.ico",
       showBeian: false,
-      subDomain: isBeta ? "099equ" : "099eqs",
+      subDomain: isBeta ? "099equ" : "099erq",
       baseUrl: isBeta ? "https://beta-api.baitech-ipfs.net/" : "https://api.baitech-ipfs.net/",
       auth: isBeta
         ? "Bearer c3Vic3RyYXRlLWNUTEJlSGlvd2JDZE1rdjNLaENSQkxzbXNmRDNicVlnVlZURU5DQlp1ZjIxRW5OOEc6MHgwMjFiNTU1OTg3ZGU4OTJlY2JlMmE5MWIzMTI3Mzg4OGIwYTUwYzZmN2ExNzAwNTFhNzVkNjAwMDc2NzhiYjA1YTU0NWIwYjJkNjVkYmRlNTJmNWQyNDU0NzljODRiMzExZDQxMjM5MjU3MzM5MTlhMGFkMzhiZWE0YjRlZGM4OQ"
-        : "Bearer c3Vic3RyYXRlLWNUSjJ3bUg5WmZuOHpLd0M2NnFQanZpRHU4M05yVmdjM0JHTG56emJNOFRSWkxhWmk6MHhiYWNlNzRhMDAwMjc5MDY3MzBlMTc5NmEyNWU2OWU2ZWRjYmIyNzg1MDEwZjVjYzYxOTg0YmQwM2I2NDk5YTE5MTY5MTEyMDdhNDNkNGFmM2MwMWEyNjRhMTRiN2UyOGQ2ZmRhYzExOTJhMmU5YmZiMTc5NjMzNGYxZTE5MGU4Mg==",
+        : "Bearer c3Vic3RyYXRlLWNUTDk4alpabU1uRlZLM2VVN3ozd3d2VUNadjNDdHJWTkY2NzRaRXBEZDRrMW5lZ2E6MHg1NDY0YTY5OGI4OTgxMjVlNzAxMTFmNjI3ZDRkYzJmYWZhNGQwMjIwNWY0MDk5MWZiNGEyNjY3MGUwMjIzNjM1MjdjN2YwMmEyYTE5ZGI0NGFmNmI0NjAxNGU5ZTE0YTMwZjlhMTliN2VlMTY3OTVkODU0ZjBmZjg2ZTUyODk4Zg==",
     };
   }
   return {
